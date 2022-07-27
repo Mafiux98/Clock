70a8658fcaaee071f25cc236ef95868758fa4c6d
Mateusz Szumlakowski <64979490+Mafiux98@users.noreply.github.com>
2022-07-27 18:04:47 +0200

    Added package.json

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 
 function printTime() {
-  let d = new Date();
-  let h = d.getHours();
-  let m = d.getMinutes();
-  let s = d.getSeconds();
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
 
-  //0 before single digit number, 2 ways
-  h = h < 10 ? "0" + h : h;
-  m = m < 10 ? "0" + m : m;
-  if (s < 10) {
-    s = "0" + s;
-  }
+    //0 before single digit number, 2 ways
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    if (s < 10) {
+        s = "0" + s;
+    }
 
-  hours.innerHTML = h + "<br><span>Hours</span>";
-  seconds.innerHTML = s + "<br><span>Seconds</span>";
-  minutes.innerHTML = m + "<br><span>Minutes</span>";
+    hours.innerHTML = h + "<br><span>Hours</span>";
+    seconds.innerHTML = s + "<br><span>Seconds</span>";
+    minutes.innerHTML = m + "<br><span>Minutes</span>";
 }
 setInterval(printTime, 1000);
--- script.test.js
+++ script.test.js
@@ -1,16 +1,16 @@
 function isInTheSameHour(currentHours, dateToTest) {
-  return currentHours.getHours() - dateToTest.getTime() < 0;
+    return currentHours.getHours() - dateToTest.getTime() < 0;
 }
 
 it("compares two Hours", () => {
-  const currentHours = new Date();
-  const hoursScript = new Date();
-  expect(isInTheSameHour(currentHours, hoursScript)).toBe(true);
+    const currentHours = new Date();
+    const hoursScript = new Date();
+    expect(isInTheSameHour(currentHours, hoursScript)).toBe(true);
 });
 
 describe("maybe", () => {
-  it("callback Time", () => {
-    const printTime = require("../printTime");
-    expect(printTime).toBeCalled();
-  });
+    it("callback Time", () => {
+        const printTime = require("../printTime");
+        expect(printTime).toBeCalled();
+    });
 });
