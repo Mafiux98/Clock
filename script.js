51bdca88ff33a8187599c4a40872240c5aa92fb9
Dariusz Wietecha <86299738+darek-gc@users.noreply.github.com>
2022-09-03 14:22:36 +0200

    Add getTime into index.html

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const getTime = require('./getTime');
-
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
